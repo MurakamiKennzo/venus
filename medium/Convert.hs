69d256f288d6a302502b49b40f18dcf70b7506c6
murakami <w18191263056@yahoo.co.jp>
2019-05-19 22:57:45 +0800

    convert

---
+++ medium/Convert.hs
@@ -0,0 +1,26 @@
+module Convert 
+  (
+    convert
+  ) where
+
+import qualified Data.Sequence as S
+import Data.Foldable (toList)
+import Data.List (transpose)
+import Data.Char (isSpace)
+
+convert :: String -> Int -> String
+convert [] _ = []
+convert s r 
+  | r <= 1 = s
+  | otherwise = filter (not . isSpace) . foldl1 (++) . transpose $ convert' (S.fromList s) 0 r 
+
+convert' :: S.Seq Char -> Int -> Int -> [String]
+convert' S.Empty _ _ = []
+convert' s i r
+  | m == 0 = 
+      let (f, l) = S.splitAt r s
+      in  toList f : convert' l (i + 1) r
+  | otherwise = 
+      let f = S.reverse $ S.update m (s `S.index` 0) $ S.replicate r ' '
+      in  toList f : convert' (S.drop 1 s) (i + 1) r
+  where m = i `mod` (r - 1)
