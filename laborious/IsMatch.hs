71860c41d699d3cf939ee857a8a113b5e90c7083
murakami <w18191263056@yahoo.co.jp>
2019-06-30 19:09:01 +0800

    isMatch desc

--- laborious/IsMatch.hs
+++ laborious/IsMatch.hs
@@ -1,3 +1,9 @@
+-- Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+
+-- '.' Matches any single character.
+-- '*' Matches zero or more of the preceding element.
+-- The matching should cover the entire input string (not partial).
+
 module IsMatch
   (
     isMatch
