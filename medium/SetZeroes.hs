3a39c06e56354fb1c07dcde0b16626385e9a9622
murakami <w18191263056@yahoo.co.jp>
2019-07-30 13:51:56 +0800

    setZeroes

--- medium/SetZeroes.hs
+++ medium/SetZeroes.hs
@@ -1,4 +1,4 @@
-Given a m x n matrix, if an element is 0, set its entire row and column to 0.
+-- Given a m x n matrix, if an element is 0, set its entire row and column to 0.
 
 module SetZeroes
   (
