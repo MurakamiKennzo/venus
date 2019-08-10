bb1b58826b0bc311dc9c1569e14a0116ae05f9c9
murakami <w18191263056@yahoo.co.jp>
2019-08-10 18:56:13 +0800

    largestRectangleArea

---
+++ laborious/LargestRectangleArea.hs
@@ -0,0 +1,22 @@
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+
+module LargestRectangleArea
+  (
+    largestRectangleArea
+  ) where
+
+largestRectangleArea :: [Int] -> Int
+largestRectangleArea = largestRectangleArea' . zip [0 ..]
+
+largestRectangleArea' :: [(Int, Int)] -> Int
+largestRectangleArea' [] = 0
+largestRectangleArea' (x:xs) = let a = largestRectangleArea'' x xs
+                                   b = largestRectangleArea' xs
+                               in  max a b
+
+largestRectangleArea'' :: (Int, Int) -> [(Int, Int)] -> Int
+largestRectangleArea'' (_, x) [] = x
+largestRectangleArea'' (i, x) ((j, y):xs) = let a = min x y
+                                                b = (j - i + 1) * a
+                                                c = largestRectangleArea'' (i, a) xs
+                                            in  max b c
