36c2391af3793bcc3acfec22a158c61d768e2084
murakami <w18191263056@yahoo.co.jp>
2019-12-03 08:08:37 +0800

    longestConsecutive

---
+++ laborious/LongestConsecutive.hs
@@ -0,0 +1,21 @@
+-- Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+
+module LongestConsecutive
+  (
+    longestConsecutive
+  ) where
+
+import Data.List ( sort
+                 , sortOn )
+
+longestConsecutive :: [Int] -> [Int]
+longestConsecutive [] = []
+longestConsecutive a = last . sortOn length . groupBy' (\a b -> succ a == b) . sort $ a
+
+groupBy' :: (a -> a -> Bool) -> [a] -> [[a]]
+groupBy' _ [] = []
+groupBy' _ [a] = [[a]]
+groupBy' f (x:y:z)
+  | f x y = (x: head a):tail a
+  | otherwise = [x] : a
+  where a = groupBy' f (y:z)
