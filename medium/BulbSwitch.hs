e18697e5bc5e7194112bcd1da66641278619f47e
murakami <w18191263056@yahoo.co.jp>
2020-06-23 11:22:44 +0800

    Update BulbSwitch.hs

--- medium/BulbSwitch.hs
+++ medium/BulbSwitch.hs
@@ -1,9 +1,9 @@
+-- There are n bulbs that are initially off. You first turn on all the bulbs. Then, you turn off every second bulb. On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the i-th round, you toggle every i bulb. For the n-th round, you only toggle the last bulb. Find how many bulbs are on after n rounds.
+
 module BulbSwitch
   (
     bulbSwitch
   ) where
-
--- There are n bulbs that are initially off. You first turn on all the bulbs. Then, you turn off every second bulb. On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the i-th round, you toggle every i bulb. For the n-th round, you only toggle the last bulb. Find how many bulbs are on after n rounds.
 
 bulbSwitch :: Int -> Int
 bulbSwitch n = length . filter (== Open) . foldl step (replicate n Close) $ [1 .. n]
