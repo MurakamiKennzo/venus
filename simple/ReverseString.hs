0f7a42e528d946ef43fc1017f968790797a5ae3b
murakami <w18191263056@yahoo.co.jp>
2020-08-10 21:08:41 +0800

    reverseString

---
+++ simple/ReverseString.hs
@@ -0,0 +1,9 @@
+-- Write a function that reverses a string. The input string is given as an array of characters char[].
+
+module ReverseString
+  (
+    reverseString
+  ) where
+
+reverseString :: String -> String
+reverseString = reverse
