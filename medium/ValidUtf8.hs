a0c333e7e8fa827a0fe58b84e29fc7a85b3bc55f
murakami <w18191263056@yahoo.co.jp>
2021-03-18 19:34:18 +0800

    validUtf8

---
+++ medium/ValidUtf8.hs
@@ -0,0 +1,43 @@
+-- A character in UTF8 can be from 1 to 4 bytes long, subjected to the following rules:
+
+-- For 1-byte character, the first bit is a 0, followed by its unicode code.
+-- For n-bytes character, the first n-bits are all one's, the n+1 bit is 0, followed by n-1 bytes with most significant 2 bits being 10.
+-- This is how the UTF-8 encoding would work:
+
+--    Char. number range  |        UTF-8 octet sequence
+--       (hexadecimal)    |              (binary)
+--    --------------------+---------------------------------------------
+--    0000 0000-0000 007F | 0xxxxxxx
+--    0000 0080-0000 07FF | 110xxxxx 10xxxxxx
+--    0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
+--    0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+-- Given an array of integers representing the data, return whether it is a valid utf-8 encoding.
+
+-- Note:
+-- The input is an array of integers. Only the least significant 8 bits of each integer is used to store the data. This means each integer represents only 1 byte of data.
+
+module ValidUtf8
+  (
+    validUtf8
+  ) where
+
+import Data.Bits
+
+validUtf8 :: [Int] -> Bool
+validUtf8 nums = validUtf8' . map (countLeadingOnesFrom 7) $ nums
+
+validUtf8' :: [Int] -> Bool
+validUtf8' [] = True
+validUtf8' (x:xs)
+  | x == 0 = validUtf8' xs
+  | x == 1 = False
+  | x > 4 = False
+  | otherwise = let (a, b) = splitAt (x - 1) xs
+                in  if all (== 1) a then validUtf8' b else False
+
+countLeadingOnesFrom :: ByteBit -> Int -> Int
+countLeadingOnesFrom n num = n - countLeadingOnesFrom' num n
+  where countLeadingOnesFrom' :: Int -> ByteBit -> ByteBit
+        countLeadingOnesFrom' num n = if testBit num n then countLeadingOnesFrom' num $ n - 1 else n
+
+type ByteBit = Int
